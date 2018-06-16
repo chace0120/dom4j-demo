3f324e696d8ab49ac508f281234043d37894b8cd
chace0120@126.com <chace0120@126.com>
2018-06-16 10:48:14 +0800

    添加带有命名空间的xml文件解析的测试代码。

--- src/test/java/Dom4jTest.java
+++ src/test/java/Dom4jTest.java
@@ -1,16 +1,15 @@
-import org.dom4j.Document;
-import org.dom4j.DocumentException;
-import org.dom4j.Element;
-import org.dom4j.Node;
+import org.apache.commons.lang3.StringUtils;
+import org.dom4j.*;
 import org.dom4j.io.SAXReader;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
-import java.io.File;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 /**
  * 测试类: 测试dom4j相关api功能
@@ -25,7 +24,7 @@ public class Dom4jTest {
     @Before
     public void init() throws DocumentException {
         SAXReader saxReader = new SAXReader();
-        document = saxReader.read(getClass().getResourceAsStream("history_forecast.xml"));
+        document = saxReader.read(getClass().getResourceAsStream("Gps_Stops.xml"));
     }
 
     @Test
@@ -46,6 +45,43 @@ public class Dom4jTest {
             Element element = (Element) node;
             Element curDate = element.element("CONSIDERED_DATE");
             System.out.println(curDate.getQualifiedName() + ":" + curDate.getTextTrim());
+        }
+    }
+
+    @Test
+    public void testIteratorAttr() {
+        Map<String, String> namespaceMap = new HashMap<>();
+        namespaceMap.put("ns", "GpsStopsByGroupReport");
+        XPath xPath = document.createXPath("//ns:table1_GroupName/ns:table1_UserNum_Collection/ns:table1_UserNum");
+        xPath.setNamespaceURIs(namespaceMap);
+        List<Node> nodes = xPath.selectNodes(document);
+        nodes.forEach(node -> {
+            System.out.println(node.valueOf("@UserName"));
+
+//            XPath otherXPath = DocumentHelper.createXPath("//ns:table1_GroupName/ns:table1_UserNum_Collection/ns:table1_UserNum");
+//            otherXPath.setNamespaceURIs(namespaceMap);
+//            List<Node> carsInfos = otherXPath.selectNodes(node);
+//            carsInfos.forEach(carsInfo -> {
+//                String interval = StringUtils.normalizeSpace(carsInfo.valueOf("@UserName"));
+//                System.out.println(interval);
+//            });
+        });
+    }
+
+    @Test
+    public void testIteratorByElement() {
+        Element root = document.getRootElement();
+
+        Iterator<Element> hotelInfos = root.elements().get(1).elements().get(0).elements().get(0).elements().get(0).elements().iterator();
+        while (hotelInfos.hasNext()) {
+            Element hotelInfo = hotelInfos.next();
+            System.out.println(hotelInfo.attributeValue("Group"));
+
+            Iterator<Element> carInfos = hotelInfo.elements().get(0).elements().iterator();
+            while (carInfos.hasNext()) {
+                Element carInfo = carInfos.next();
+                System.out.println(carInfo.attributeValue("UserName"));
+            }
         }
     }
 }
